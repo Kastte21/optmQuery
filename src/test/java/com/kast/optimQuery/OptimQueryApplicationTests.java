9f724ed5e07142245654768eb898739994979e51
Kastte21 <108232886+Kastte21@users.noreply.github.com>
2025-07-10 10:48:02 -0500

    Initial project structure

---
+++ src/test/java/com/kast/optimQuery/OptimQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kast.optimQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
